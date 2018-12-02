c9d71baca5886a544dfc341a213af83aad0ded58
Niklas <niklas.b2013@gmail.com>
2018-12-02 11:42:20 +0100

    Main-Klasse und Konstanten-Interface hinzufügen

---
+++ src/control/Konstanten.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author Niklas
+ */
+public interface Konstanten 
+{
+    public static final String INFORMATIONS_TEXT = String.format("Guten Tag, %Sdieses Programm liest den Text einer Datei ein und gibt die enthaltenen Worte zurück. %S", "\n", "\n");
+}
---
+++ src/control/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author Niklas
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        informieren();
+    }
+
+    private static void informieren() 
+    {
+        System.out.println(Konstanten.INFORMATIONS_TEXT);
+    }
+}
